3fa38604c2f8e0944891fb490b295eddc0c35e05
WanglerIV <109871315+WanglerIV@users.noreply.github.com>
2024-05-06 23:19:24 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  // Math.js'i external olarak belirt
-  optimizeDeps: {
-    exclude: ['mathjs']
+  build: {
+    rollupOptions: {
+      external: ['mathjs']
+    }
   }
 });
