24aa7903acc0999545093cb215432732c86439a6
WanglerIV <109871315+WanglerIV@users.noreply.github.com>
2024-05-06 23:14:45 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,9 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  // Vite eklentilerini burada tanımlayın
-  plugins: [
-    // React eklentisini kullanmak için
-    react(),
-  ],
-
-  // Harici modüllerin tanımlandığı bölüm
+  plugins: [react()],
+  // Math.js'i external olarak belirt
   optimizeDeps: {
-    // Harici kütüphaneleri buraya ekleyin
-    include: ['mathjs'],
-  },
+    exclude: ['mathjs']
+  }
 });
